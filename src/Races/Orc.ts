726fe1ba27cafea1129147e69ff0c1627e608d5b
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-18 18:13:44 -0300

    feat: there is a Orc class that extends Race abstract class

---
+++ src/Races/Orc.ts
@@ -0,0 +1,29 @@
+import Race from './Race';
+
+class Orc extends Race {
+  static count = 0;
+  private _maxLifePoints: number;
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    Orc.count += 1;
+    this._maxLifePoints = 74;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Orc.count;
+  }
+
+  get name(): string {
+    return this.name;
+  }
+
+  get dexterity(): number {
+    return this.dexterity;
+  }
+}
+
+export default Orc;
