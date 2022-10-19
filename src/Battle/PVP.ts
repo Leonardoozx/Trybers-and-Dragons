9822ad64819c8fc08ff3c15cd5c7cc211e5ce758
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-19 01:57:20 -0300

    feat: there is a PVP class for making player battle against other player

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,20 @@
+import Character from '../Character';
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  constructor(
+    private _playerOne: Character | Fighter,
+    private _playerTwo: Character | Fighter,
+  ) {
+    super(_playerOne);
+  }
+
+  fight(): number {
+    this._playerOne.attack(this._playerTwo);
+    this._playerTwo.attack(this._playerOne);
+    return super.fight();
+  }
+}
+
+export default PVP;
